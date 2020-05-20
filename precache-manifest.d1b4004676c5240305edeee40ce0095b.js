b15c20cfb8c0dafc466e39ba94d4a285f35c18ec
vpidkivka <vpidkivka@eliteit.org>
2020-05-20 20:29:53 +0300

    Updates

---
+++ precache-manifest.d1b4004676c5240305edeee40ce0095b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3a00b894294b6ed4b66a9399e9ce26",
+    "url": "/test-redentu/index.html"
+  },
+  {
+    "revision": "f2cee642d4b9b3a7f0cd",
+    "url": "/test-redentu/static/css/main.a122c8f8.chunk.css"
+  },
+  {
+    "revision": "faefeac58a9d3c54239d",
+    "url": "/test-redentu/static/js/2.46db9b62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-redentu/static/js/2.46db9b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2cee642d4b9b3a7f0cd",
+    "url": "/test-redentu/static/js/main.214e7e86.chunk.js"
+  },
+  {
+    "revision": "aeae638f17d80f0de718",
+    "url": "/test-redentu/static/js/runtime-main.ad826a4c.js"
+  }
+]);
